7920873fb218c952a120a75f3bf395bf174aaf3e
Nikhila D <109202601+Nikhila-DN@users.noreply.github.com>
2024-03-28 19:11:15 +0530

    Made `different files` for each Component

---
+++ Components/Footer.js
@@ -0,0 +1,12 @@
+// Footer component for footer section
+const Footer = () => {
+  return (
+    <div className="footer">
+      <strong>
+        Footer
+      </strong>
+    </div>
+  );
+};
+
+export default Footer;
